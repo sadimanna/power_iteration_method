050566b28b795dafac788b6ea8a43b74556353ef
Siladittya Manna <sadimanna@users.noreply.github.com>
2018-01-30 12:48:19 +0530

    Update and rename pi.py to power_iteration.py

--- power_iteration.py
+++ power_iteration.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.sparse as sp
 import time
 
-def power_iteration(A, num_simulations):
+def power_iteration(A, niter):
 	tol = 10**(-9)
 	Ashape = A.get_shape()
         eigvec = np.random.rand(Ashape[0])
@@ -14,7 +14,7 @@ def power_iteration(A, num_simulations):
         	eigvec1_norm = np.linalg.norm(eigvec1)
         	# re normalize the vector
         	eigvec = eigvec1 / eigvec1_norm
-		
+		#eigenvalue
 		eigval_new = np.dot(np.transpose(eigvec),A.dot(eigvec))/np.dot(np.transpose(eigvec),eigvec)
 		if (abs(eigval_new-eigval_old)/eigval_new) < tol:
 			return eigval_new
@@ -24,15 +24,4 @@ def power_iteration(A, num_simulations):
 
 def get_maxeigval(A):
 	niter = 200000
-	#maxeigval = power_iteration(A,niter)
-	#UNSTABLE
-	#rc = np.array([i for i in xrange(Ashape[0])])
-	#Atemp = A - maxeigval*sp.coo_matrix((np.ones(Ashape[0]),(rc,rc)),shape=Ashape)
-	#mineigval = power_iteration(Atemp,niter) + maxeigval
-
 	return power_iteration(A,niter)
-
-#stime=time.time()
-#A = sp.load_npz('ham_kin.npz')
-#print 'Time to load..'+str(time.time()-stime)+' seconds...'
-#print 'Total time...'+str(time.time()-stime)+' seconds...'
